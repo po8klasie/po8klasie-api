0b0f54ad5bc374dcd4adc18212fbad4d6bb1e009
annkamsk <an.kramarska@gmail.com>
2020-01-26 00:48:55 +0100

    Created endpoint for public schools

--- search/views.py
+++ search/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+
+from search.serializers import *
 
 
 def index(request):
     return HttpResponse("Hello")
+
+
+@api_view(['GET'])
+def public_school_list(request):
+    if request.method == 'GET':
+        public_schools = PublicSchool.objects.all()
+        serializer = PublicSchoolSerializer(public_schools, many=True)
+        return JsonResponse(serializer.data, safe=False)
