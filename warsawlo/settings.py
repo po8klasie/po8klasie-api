ead54e243202b2fde8fd3fc0859edf66dee29224
annkamsk <an.kramarska@gmail.com>
2020-02-15 21:32:07 +0100

    Static dirs changed

--- warsawlo/settings.py
+++ warsawlo/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'warsawlo/static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
