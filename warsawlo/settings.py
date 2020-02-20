73182a237b5b9548ab473dcb8b36c8e41046d492
annkamsk <an.kramarska@gmail.com>
2020-02-20 22:20:30 +0100

    Move static dir back to warsawlo level

--- warsawlo/settings.py
+++ warsawlo/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'warsawlo/static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
