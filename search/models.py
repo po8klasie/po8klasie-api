8010ac429cca6e0d22f07cd8e6471e6f7e9b02b3
Anna Kramarska <ak@qed.ai>
2020-03-06 23:05:33 +0100

    Filter each endpoint against all of the class fields

--- search/models.py
+++ search/models.py
@@ -13,11 +13,17 @@ class Address(models.Model):
     longitude = models.FloatField(null=True)
     latitude = models.FloatField(null=True)
 
+    def __str__(self):
+        return ','.join([self.city, self.district, self.street, self.building_nr])
+
 
 class ContactData(models.Model):
     website = models.CharField(max_length=100, null=True)
     phone = models.CharField(max_length=20, null=True)
     email = models.CharField(EmailValidator(), max_length=100, null=True)
+
+    def __str__(self):
+        return ','.join([self.website, self.phone, self.email])
 
 
 class PublicInstitutionData(models.Model):
@@ -32,6 +38,9 @@ class PublicInstitutionData(models.Model):
     institution_regon = models.CharField(max_length=14)
     regon = models.CharField(max_length=14)
     data = JSONField(null=True)
+
+    def __str__(self):
+        return ','.join([self.short_name, self.institution_short_name, self.institution_nr])
 
 
 class PrivateInstitutionData(models.Model):
@@ -54,6 +63,9 @@ class School(models.Model):
     private_institution_data = models.ForeignKey(PrivateInstitutionData, on_delete=models.SET_NULL, null=True)
     data = JSONField(null=True)
 
+    def __str__(self):
+        return ','.join([self.school_name, self.school_type_generalised])
+
 
 class HighSchoolClass(models.Model):
     # O - ogólnokształcące, MS - mistrzostwa sportowego
@@ -61,6 +73,9 @@ class HighSchoolClass(models.Model):
     name = models.CharField(max_length=200)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     year = IntegerRangeField()
+
+    def __str__(self):
+        return ','.join([self.type, self.name])
 
 
 class Language(models.Model):
@@ -78,6 +93,9 @@ class Language(models.Model):
     is_bilingual = models.BooleanField(default=False)
     multiple_levels = models.BooleanField(default=False)
 
+    def __str__(self):
+        return ','.join([self.name, self.high_school_class.school.school_name])
+
 
 class ExtendedSubject(models.Model):
     subjects = [('biol', 'biologia'), ('chem', 'chemia'), ('filoz', 'filozofia'), ('fiz', 'fizyka'),
@@ -88,6 +106,9 @@ class ExtendedSubject(models.Model):
 
     high_school_class = models.ForeignKey(HighSchoolClass, on_delete=models.CASCADE)
     name = models.CharField(choices=subjects + Language.languages, max_length=40)
+
+    def __str__(self):
+        return ','.join([self.name, self.high_school_class.school.school_name])
 
 
 class Statistics(models.Model):
--- search/views.py
+++ search/views.py
@@ -1,76 +1,64 @@
-from django.http import JsonResponse
 from rest_framework import viewsets
-from django.contrib.postgres.search import TrigramSimilarity
-from django_filters import rest_framework as filters
-
 from search.serializers import *
 
-
-# class HighSchoolViewSet(viewsets.ViewSet):
-#
-#     def list(self, request, format=None):
-#         if request.GET.get('name') is not None:
-#             name = request.GET.get('name')
-#             schools = School.objects \
-#                 .filter(school_type='liceum ogólnokształcące') \
-#                 .annotate(similarity=TrigramSimilarity('school_name', name)) \
-#                 .filter(similarity__gte=0.06) \
-#                 .order_by('-similarity')
-#         else:
-#             schools = School.objects \
-#                 .filter(school_type='liceum ogólnokształcące')
-#
-#         serializer = SchoolSerializer(schools, many=True)
-#         return JsonResponse(serializer.data, safe=False)
 
 class SchoolViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = School.objects.all()
     serializers = SchoolSerializer
     serializer_class = SchoolSerializer
+    filterset_fields = [f.name for f in School._meta.fields if f.name not in ['specialised_divisions', 'data']]
 
 
 class HighSchoolViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = School.objects.filter(school_type='liceum ogólnokształcące')
     serializer_class = SchoolSerializer
-    filterset_fields = ['is_public']
-    search_fields = ['school_name']
+    filterset_fields = [f.name for f in School._meta.fields if f.name not in ['specialised_divisions', 'data']]
+    search_fields = ['school_name', 'school_type']
 
 
 class AddressViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+    filterset_fields = [f.name for f in Address._meta.fields]
 
 
 class ContactViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ContactData.objects.all()
     serializer_class = ContactSerializer
+    filterset_fields = [f.name for f in ContactData._meta.fields]
 
 
 class PublicInstitutionDataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PublicInstitutionData.objects.all()
     serializer_class = PublicInstitutionDataSerializer
+    filterset_fields = [f.name for f in PublicInstitutionData._meta.fields if f.name not in ['data']]
 
 
 class PrivateInstitutionDataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PrivateInstitutionData.objects.all()
     serializer_class = PrivateInstitutionDataSerializer
+    filterset_fields = [f.name for f in PrivateInstitutionData._meta.fields]
 
 
 class HighSchoolClassViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = HighSchoolClass.objects.all()
     serializer_class = HighSchoolClassSerializer
+    filterset_fields = [f.name for f in HighSchoolClass._meta.fields if f.name not in ['year']]
 
 
 class ExtendedSubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ExtendedSubject.objects.all()
     serializer_class = ExtendedSubjectSerializer
+    filterset_fields = [f.name for f in ExtendedSubject._meta.fields]
 
 
 class LanguageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    filterset_fields = [f.name for f in Language._meta.fields]
 
 
 class StatisticsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Statistics.objects.all()
     serializer_class = StatisticsSerializer
+    filterset_fields = [f.name for f in Statistics._meta.fields]
