8d9bd0279c4b55a9c40e8fdf7b759633bede6448
konradkrasno <konradkrasno@gmail.com>
2021-04-17 21:43:36 +0200

    Revert insert_profile_2020 and add updating school and insert scores from 2020

--- search/tests.py
+++ search/tests.py
@@ -1,10 +1,1 @@
-import pytest
-
-from search.load_data import insert_publiczne_oddzialy_2020
-from search.models import School
-
-
-@pytest.mark.django_db
-def test_insert_publiczne_oddzialy_2020():
-    insert_publiczne_oddzialy_2020()
-    assert School.objects.all().count() == 1044
+from django.test import TestCase
